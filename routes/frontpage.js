10b70a47505797900efecd0c74c7aa7cae3e04a9
Yuriy Kucherenko <yuriy.kycherenko@gmail.com>
2014-10-26 17:07:10 +0200

    Change index.html and fix indents

--- routes/frontpage.js
+++ routes/frontpage.js
@@ -2,5 +2,5 @@
  * Created by Fyfar on 25.10.2014.
  */
 exports.get = function (req, res, next) {
-	res.render('index');
+  res.render('index');
 };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var HttpError = require('../error').HttpError;
 
 module.exports = function (app) {
-	app.get('/', require('./frontpage').get);
-	app.get('/error', require('./error').get);
-	app.get('*', function (req, res, next) {
-		next(new HttpError(404, 'Page not found!'));
-	})
+  app.get('/', require('./frontpage').get);
+  app.get('/error', require('./error').get);
+  app.get('*', function (req, res, next) {
+    next(new HttpError(404, 'Page not found!'));
+  })
 };
