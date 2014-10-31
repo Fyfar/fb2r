3c358fffc521fb3d66ccacca7b067cfb1671fd3e
RomanMoskalenko <13moskalenko.roman@gmail.com>
2014-11-01 00:03:27 +0200

    improvements

---
+++ public/js/js.js
@@ -0,0 +1,92 @@
+			
+			
+			function getTheme() {
+				if(localStorage.getItem('theme') === 'false') {
+					$('body').addClass('night');
+					$('.content, .pageTitle, .backToReading').addClass('night');		
+				}
+				else {
+					$('body').removeClass('night');
+					$('.content, .pageTitle, .backToReading').removeClass('night');
+				}
+			}
+			
+			function getSidebar() {
+				if(localStorage.getItem('asideVisi') === 'true') {
+					$('aside').addClass("asideVisible");
+					$('.logo').attr("src","../public/img/arrow_reverse.png");
+				
+				}
+				else {					
+					$('aside').removeClass("asideVisible");
+					$('.logo').attr("src","../public/img/arrow.png");	
+					
+				}
+			}
+			
+			function changeTheme() {
+				if(localStorage.getItem('theme') === 'false') {
+					$('body').removeClass('night');
+					$('.content, .pageTitle, .backToReading').removeClass('night');
+					localStorage.setItem('theme', 'true'); 
+					
+				}
+				else {
+				
+				$('body').addClass('night');
+					$('.content, .pageTitle, .backToReading').addClass('night');					
+					localStorage.setItem('theme', 'false'); 
+					
+				}
+				
+			}
+			
+			$('document').ready(function(){			
+			
+				getTheme();
+				getSidebar();
+				
+				$('.nd').click(function(){
+					changeTheme();
+				});
+				$('.fullscreen').click(function(){
+					document.documentElement.requestFullScreen();
+				});
+				$('.logo').click(function() {
+					sidebar();
+				});
+				
+			});
+			
+			function fullscreen(element) {
+			  if(element.requestFullScreen) {
+				element.requestFullScreen();
+			  } else if(element.mozRequestFullScreen) {
+				element.mozRequestFullScreen();
+			  } else if(element.webkitRequestFullScreen) {
+				element.webkitRequestFullScreen();
+			  }
+			}
+			function addBookmark() {
+				$(".formBookmark").addClass("addBookmark");
+			}
+			function showInfoBook() {
+				$(".formBookInfo").addClass("showBookInfo");
+			}
+			
+			function sidebar() {
+				if(localStorage.getItem('asideVisi') === 'true') {					
+					$('aside').removeClass("asideVisible");
+					$('.logo').attr("src","../public/img/arrow.png");					
+					localStorage.setItem('asideVisi', 'false');
+					
+				}
+				else {
+					$('aside').addClass("asideVisible");
+					$('.logo').attr("src","../public/img/arrow_reverse.png");					
+					localStorage.setItem('asideVisi', 'true');
+					
+				}
+			}
+			
+			
